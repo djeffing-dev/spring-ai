9471c7688dc22b46917db2eb559dae6eb1f01efa
DJEFFING <tsafackjefferson2001@gmail.com>
2025-09-25 17:50:26 -0400

    modification du fichier webConfig pour y ajouter des adresses qui doivent etre prise en compte par le serveur. j'ai egalement commentre les insertions qui se trouve dans le fichier data.sql

--- src/main/java/com/djeffing/spring_ai/configs/securities/WebConfig.java
+++ src/main/java/com/djeffing/spring_ai/configs/securities/WebConfig.java
@@ -28,6 +28,7 @@ public class WebConfig {
         configuration.addAllowedOrigin("http://192.168.4.33:4200");
         configuration.addAllowedOrigin("https://angular-ai-web-taky-git-main-jeffingdevs-projects.vercel.app/");
         configuration.addAllowedOrigin("https://angular-ai-web-taky.vercel.app/");
+        configuration.addAllowedOrigin("https://virbray-ai.com/");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
 
